f27d2b554a514e6f0bade497d30aa73e64993fff
Ubuntu <ubuntu@ip-172-31-13-55.us-east-2.compute.internal>
2023-09-28 20:18:46 +0000

    adding quiz3 and quiz2

---
+++ quiz-2/main.rb
@@ -0,0 +1,12 @@
+#5mails, sort the emails by alphabetical order
+#how do I clone my git repository on repl it.
+#how do i print my current working directory, and create 3 files using BASH..
+emails = [
+  "123@gmail.com",
+  "1123@gmail.com",
+  "2123@gmail.com",
+  "3123@gmail.com", 
+  "4123@gmail.com"
+]
+sorted_emails = emails.sort
+puts sorted_emails
